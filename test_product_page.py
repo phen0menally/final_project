12127dabb896265959f98a24d0a37f0285f4ebe2
phen0menally <tim.abrikos@yandex.ru>
2022-06-20 13:16:29 +0300

    Add parametrize for test

--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,18 @@
+import time
+
+import pytest
 from selenium.webdriver.common.by import By
 
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo_offer', ["0", "1", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    browser.delete_all_cookies()
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_correct_product_name()
     page.should_be_correct_adding_product_price()
-
