096a9cf7a2ffd738ad32a171ce4b8e48d74b8a33
phen0menally <tim.abrikos@yandex.ru>
2022-06-20 13:17:28 +0300

    Add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class CartPageLocators():
+    ADD_TO_CART = (By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
+    BOOK_NAME = (By.CSS_SELECTOR, '[class="active"]')
+    BOOK_NAME_AFTER_ADD = (By.CSS_SELECTOR, '.alertinner strong')
+    BOOK_PRICE = (By.CSS_SELECTOR, '[class="col-sm-6 product_main"] > [class="price_color"]')
+    CART_PRICE = (By.CSS_SELECTOR, '.alert-info .alertinner strong')
+
