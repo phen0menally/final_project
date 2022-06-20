10d8c5aeab96df5698a1a529a74be383d4a463fb
phen0menally <tim.abrikos@yandex.ru>
2022-06-20 15:20:36 +0300

    Relocate functions to base_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,9 +5,4 @@ from .login_page import LoginPage
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-
-    def should_be_login_link(self):
-        assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    pass
