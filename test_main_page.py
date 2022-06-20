e0e6fff04b6cddf2559adacc8e20809150eeb804
phen0menally <tim.abrikos@yandex.ru>
2022-06-20 16:23:45 +0300

    Add test interacting with the basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
+import pytest
 from selenium.webdriver.common.by import By
 
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -30,3 +32,12 @@ def test_should_see_login_and_registration_form(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_be_empty_basket()
+    page.should_be_message_basket()
