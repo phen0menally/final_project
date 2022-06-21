c6345dad473852a1d182f9cb282965e6f045331a
phen0menally <tim.abrikos@yandex.ru>
2022-06-21 16:21:00 +0300

    Add new func register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from .locators import RegistrationPageLocators
 
 
 class LoginPage(BasePage):
@@ -12,9 +13,15 @@ class LoginPage(BasePage):
         assert "login" in self.url, "It is not login page"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Not Found Login Form"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "Not Found Register Form"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*RegistrationPageLocators.EMAIL).send_keys(email)
+        self.browser.find_element(*RegistrationPageLocators.PASS1).send_keys(password)
+        self.browser.find_element(*RegistrationPageLocators.PASS2).send_keys(password)
+        self.browser.find_element(*RegistrationPageLocators.REG_BTN).click()
+
+
