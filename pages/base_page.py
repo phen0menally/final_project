521b3b4a35d4596461693589bc0029d60b2e17d6
phen0menally <tim.abrikos@yandex.ru>
2022-06-21 16:20:15 +0300

    Add authorization check

--- pages/base_page.py
+++ pages/base_page.py
@@ -68,3 +68,6 @@ class BasePage():
         link = self.browser.find_element(*BasketPageLocators.BASKET)
         link.click()
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
